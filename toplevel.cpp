16a8421176c9fffd4e92460157661531430428a4
P Imbrasas <pi514@york.ac.uk>
2016-04-14 17:14:34 +0100

    Working, optimised A*

--- toplevel.cpp
+++ toplevel.cpp
@@ -8,61 +8,11 @@
 #define OPEN 1
 #define CLOSED 2
 
-int manhattan(point_t p1, point_t p2) {
-	return p2.x - p1.x + p2.y - p1.y;
-}
-
 world_t world;
+holder_t o;
 
-// [x][y]
-uint2 grid[MAX_WORLD_SIZE][MAX_WORLD_SIZE];
-
-typedef struct __attribute__((packed)) {
-	point_t point = {0, 0};
-	uint12 cost = 0;
-	uint2 status = UNVISITED; // 0 - unvisited, 1 - open, 2 - closed
-	bool exist = false;
-	uint2 type = NOTHING;
-} node_t;
-
-typedef struct __attribute__((packed)) {
-	int size_closed;
-	int size_open;
-	node_t nodes[MAX_WORLD_SIZE][MAX_WORLD_SIZE];
-} l_t;
-
-l_t o;
-
-bool is_empty(bool closed) {
-	if (closed) return o.size_closed;
-	else return o.size_open;
-}
-
-int in_list(node_t n, bool closed, point_t *p) {
-	for (int x = 0; x < MAX_WORLD_SIZE; x++) {
-		for (int y = 0; y < MAX_WORLD_SIZE; y++) {
-			if (closed) {
-				if (o.nodes[x][y].exist && o.nodes[x][y].status == CLOSED) {
-					if (o.nodes[x][y].point.x == n.point.x && o.nodes[x][y].point.y == n.point.y) {
-						p->x = x;
-						p->y = y;
-						return 1;
-					}
-				}
-			} else {
-				if (o.nodes[x][y].exist) {
-					if (o.nodes[x][y].point.x == n.point.x && o.nodes[x][y].point.y == n.point.y) {
-						p->x = x;
-						p->y = y;
-						return 1;
-					}
-				}
-			}
-
-		}
-	}
-
-	return -1;
+int manhattan(point_t *p1, point_t *p2) {
+	return p2->x - p1->x + p2->y - p1->y;
 }
 
 void set_unvisited(uint8 x, uint8 y) {
@@ -72,41 +22,29 @@ void set_unvisited(uint8 x, uint8 y) {
 void set_closed(uint8 x, uint8 y) {
 	o.nodes[x][y].status = CLOSED;
 	o.size_closed++;
+	o.size_open--;
 }
 
 void set_open(uint8 x, uint8 y) {
 	o.nodes[x][y].status = OPEN;
 	o.size_open++;
+	o.size_closed--;
 }
 
-void change_cost(uint8 x, uint8 y, uint12 new_cost) {
-	o.nodes[x][y].cost = new_cost;
-}
+void check_neighbor(uint8 x, uint8 y, node_t *n, bool check) {
+	if (check && o.nodes[x][y].type != WALL) {
+		unsigned short new_cost = n->cost + 1;
 
-void add(uint8 x, uint8 y) {
-	o.nodes[x][y].exist = true;
-}
-
-void remove(uint8 x, uint8 y) {
-	o.nodes[x][y].exist = false;
-}
-
-void check_neighbor(uint8 x, uint8 y, node_t n) {
-	// For the points that are north, south, east and west from n.point (and that are not walls)
-	if (x < world.size && grid[x][y] != WALL) {
-		// Create a node m with the neighbour point and cost n.cost + 1
-		uint8 x = (uint8) (x);
-		uint8 y = y;
-		node_t m = {{x, y}, n.cost + 1};
-
-		if (!o.nodes[x][y].status != CLOSED) { // If the neighbour point is not in a node in closed
-
-			// If the point is in open but has a higher cost than m.cost
-			if (o.nodes[x][y].status == OPEN && o.nodes[x][y].cost > m.cost) {
-				change_cost(x, y, m.cost); // replace the cost with m.cost
-			} else if (o.nodes[x][y].status != OPEN) { // If the point is not in open, add m to open
-				o.nodes[x][y].status = OPEN;
+		switch (o.nodes[x][y].status) {
+		case OPEN: // if open then potentialy change the cost with the new cost as we found a shorter route
+			if (o.nodes[x][y].status == OPEN && o.nodes[x][y].cost > new_cost) {
+				o.nodes[x][y].cost = new_cost; // replace the cost with m.cost
 			}
+			break;
+		case UNVISITED: // if unvisited, mark it as open and set the cost to it
+			set_open(x, y);
+			o.nodes[x][y].cost = new_cost;
+			break;
 		}
 	}
 }
@@ -117,9 +55,8 @@ int a_star_2(point_t start, point_t end) {
 
 	// Initialise open to contain a node of start, with a cost of 0
 	set_open(start.x, start.y);
-	add(start.x, start.y);
 
-	while (!is_empty(false)) { // Whilst there are nodes in the open list
+	while (o.size_open > 0) { // Whilst there are nodes in the open list
 		point_t min_heuristic_index;
 		int min_heuristic = 100000000;
 
@@ -127,9 +64,10 @@ int a_star_2(point_t start, point_t end) {
 		for (int x = 0; x < MAX_WORLD_SIZE; x++) {
 			for (int y = 0; y < MAX_WORLD_SIZE; y++) {
 				if (o.nodes[x][y].status == OPEN) {
-					int heuristic = o.nodes[x][y].cost + manhattan(o.nodes[x][y].point, end);
+					int heuristic = o.nodes[x][y].cost + manhattan(&o.nodes[x][y].point, &end);
 					if (heuristic < min_heuristic) {
-						min_heuristic_index = {x, y};
+						min_heuristic_index.x = x;
+						min_heuristic_index.y = y;
 						min_heuristic = heuristic;
 					}
 				}
@@ -138,7 +76,7 @@ int a_star_2(point_t start, point_t end) {
 
 		// Remove n from open and add n to closed
 		node_t n = o.nodes[min_heuristic_index.x][min_heuristic_index.y];
-		set_closed(min_heuristic_index.x, min_heuristic_index.y);
+		set_closed(n.point.x, n.point.y);
 
 		// If n.point is end then the search is complete. n.cost is the length of the path.
 		if (n.point.x == end.x && n.point.y == end.y) {
@@ -147,10 +85,10 @@ int a_star_2(point_t start, point_t end) {
 		}
 
 		// For the points that are north, south, east and west from n.point (and that are not walls)
-		check_neighbor(n.point.x + 1, n.point.y, n);
-		check_neighbor(n.point.x - 1, n.point.y, n);
-		check_neighbor(n.point.x, n.point.y + 1, n);
-		check_neighbor(n.point.x, n.point.y - 1, n);
+		check_neighbor(n.point.x + 1, n.point.y, &n, n.point.x + 1 < world.size);
+		check_neighbor(n.point.x - 1, n.point.y, &n, n.point.x - 1 >= 0);
+		check_neighbor(n.point.x, n.point.y + 1, &n, n.point.y + 1 < world.size);
+		check_neighbor(n.point.x, n.point.y - 1, &n, n.point.y - 1 >= 0);
 	}
 
 	return -1;
@@ -161,6 +99,16 @@ void toplevel(hls::stream<uint32> &input, hls::stream<uint32> &output) {
 #pragma HLS RESOURCE variable=input core=AXI4Stream
 #pragma HLS RESOURCE variable=output core=AXI4Stream
 #pragma HLS INTERFACE ap_ctrl_none port=return
+
+	for (int x = 0; x < MAX_WORLD_SIZE; x++) {
+		for (int y = 0; y < MAX_WORLD_SIZE; y++) {
+			o.nodes[x][y].cost = 0;
+			o.nodes[x][y].point.x = x;
+			o.nodes[x][y].point.y = y;
+			o.nodes[x][y].status = UNVISITED;
+			o.nodes[x][y].type = NOTHING;
+		}
+	}
 
 	world.size = input.read();
 	world.walls_size = input.read();
@@ -174,7 +122,7 @@ void toplevel(hls::stream<uint32> &input, hls::stream<uint32> &output) {
 		wt.length = (uint8) ((in >> 24) & 0xFF);
 
 		world.walls[i] = wt;
-		grid[wt.x][wt.y] = WALL;
+		o.nodes[wt.x][wt.y].type = WALL;
 	}
 
 	world.waypoints_size = input.read();
@@ -186,7 +134,7 @@ void toplevel(hls::stream<uint32> &input, hls::stream<uint32> &output) {
 		wt.y = (uint8) ((in >> 8) & 0xFF);
 
 		world.waypoints[i] = wt;
-		grid[wt.x][wt.y] = WAYPOINT;
+		o.nodes[wt.x][wt.y].type = WAYPOINT;
 	}
 
 	// create walls in grid2
@@ -195,14 +143,14 @@ void toplevel(hls::stream<uint32> &input, hls::stream<uint32> &output) {
 		case 0: // horizontal
 			for (int j = 0; j < world.walls[i].length; j++) {
 				if (world.walls[i].x + j < world.size) {
-					grid[world.walls[i].x + j][world.walls[i].y] = WALL;
+					o.nodes[world.walls[i].x + j][world.walls[i].y].type = WALL;
 				}
 			}
 			break;
 		case 1: // vertical
 			for (int j = 0; j < world.walls[i].length; j++) {
 				if (world.walls[i].y + j < world.size) {
-					grid[world.walls[i].x][world.walls[i].y + j] = WALL;
+					o.nodes[world.walls[i].x][world.walls[i].y + j].type = WALL;
 				}
 			}
 			break;
--- toplevel.h
+++ toplevel.h
@@ -5,8 +5,54 @@
 #include <stdlib.h>
 #include <ap_int.h>
 #include <hls_stream.h>
-#include "list.h"
-#include "defs.h"
+
+#define MAX_SIZE 3600
+#define MAX_WORLD_SIZE 60
+
+//Typedefs
+typedef ap_uint<32> uint32;
+typedef ap_int<32> int32;
+typedef ap_uint<2> uint2;
+typedef ap_uint<12> uint12;
+typedef unsigned char uint8;
+
+typedef struct __attribute__((packed)) {
+	uint8 x;
+	uint8 y;
+} point_t;
+
+typedef struct __attribute__((packed)) {
+	point_t point;
+	unsigned short cost;
+	unsigned char status; // 0 - unvisited, 1 - open, 2 - closed
+	unsigned char type;
+} node_t;
+
+typedef struct __attribute__((packed)) {
+	int size_closed;
+	int size_open;
+	node_t nodes[MAX_WORLD_SIZE][MAX_WORLD_SIZE];
+} holder_t;
+
+typedef struct __attribute__((packed)) {
+	uint8 x;
+	uint8 y;
+} waypoint_t;
+
+typedef struct __attribute__((packed)) {
+	uint8 x;
+	uint8 y;
+	uint8 direction; // 0 horizontal; 1 vertical
+	uint8 length;
+} wall_t;
+
+typedef struct __attribute__((packed)) {
+	uint8 size;
+	uint8 waypoints_size;
+	waypoint_t waypoints[12];
+	uint8 walls_size;
+	wall_t walls[20];
+} world_t;
  
 //Prototypes
 void toplevel(hls::stream<uint32> &input, hls::stream<uint32> &output);
