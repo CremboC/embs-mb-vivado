11f9e2cbf000105d186035a1506480a129ca4b7c
P Imbrasas <pi514@york.ac.uk>
2016-04-29 13:25:31 +0100

    Reduce large world time by 20 seconds, medium is now half a second to instant

--- toplevel.cpp
+++ toplevel.cpp
@@ -41,6 +41,7 @@ void set_open(uint8 x, uint8 y) {
  * The direction is the relative direction of the parent node. This is used to get the full path later on.
  */
 void check_neighbor(uint8 x, uint8 y, node_t &n, bool check, uint2 direction) {
+#pragma HLS PIPELINE
 	if (check && o.nodes[x][y].type != WALL) {
 		uint14 new_cost = n.cost + 1;
 
@@ -71,13 +72,16 @@ int a_star(waypoint_t start, waypoint_t end) {
 	// Initialise open to contain a node of start, with a cost of 0
 	set_open(start.x, start.y);
 
-	while (o.size_open > 0) { // Whilst there are nodes in the open list
+	a_star_loop: while (o.size_open > 0) { // Whilst there are nodes in the open list
+#pragma HLS LOOP_TRIPCOUNT min=1 max=3600
 		point_t min_h;
 		int min_heuristic = 100000000;
 
 		// Find the node (n) in open with the lowest n.cost + manhattan(n.point, end)
-		for (int x = 0; x < MAX_WORLD_SIZE; x++) {
-			for (int y = 0; y < MAX_WORLD_SIZE; y++) {
+		find_manhattan_x: for (int x = 0; x < world.size; x++) {
+#pragma HLS LOOP_TRIPCOUNT min=10 max=60
+			find_manhattan_y: for (int y = 0; y < world.size; y++) {
+#pragma HLS LOOP_TRIPCOUNT min=10 max=60
 				if (o.nodes[x][y].status == OPEN) {
 					int heuristic = o.nodes[x][y].cost + manhattan(x, y, end.x, end.y);
 					if (heuristic < min_heuristic) {
@@ -112,8 +116,11 @@ int a_star(waypoint_t start, waypoint_t end) {
  * Helper to reset the world in between the A* runs.
  */
 void reset_world() {
-	for (int x = 0; x < MAX_WORLD_SIZE; x++) {
-		for (int y = 0; y < MAX_WORLD_SIZE; y++) {
+	reset_world_x: for (int x = 0; x < world.size; x++) {
+#pragma HLS LOOP_TRIPCOUNT min=10 max=60
+		reset_world_y: for (int y = 0; y < world.size; y++) {
+#pragma HLS LOOP_TRIPCOUNT min=10 max=60
+#pragma HLS LOOP_FLATTEN
 			o.nodes[x][y].cost = 0;
 			o.nodes[x][y].status = UNVISITED;
 		}
@@ -131,7 +138,7 @@ uint14 sum_distance(uint8 *path) {
 	// loop through all the waypoints in the path
 	// loop until waypoints_size - 2 because we also increment the index by one to get the
 	// "to" point
-	for (uint8 i = 0; i < world.waypoints_size - 2; i++) {
+	sum_waypoints: for (uint8 i = 0; i < world.waypoints_size - 2; i++) {
 		uint8 from_index = path[i];
 		uint8 to_index = path[i + 1];
 
@@ -159,7 +166,7 @@ uint14 find_min_cost(uint8 *solution) {
 
 	int tmp, i, j; // Upper Index i; Lower Index j
 
-	for (int i = 0; i < iterations; i++) {  // initialise arrays; a[N] can be any type
+	init_permutations: for (int i = 0; i < iterations; i++) {  // initialise arrays; a[N] can be any type
 		permutation[i] = i + 1;   // a[i] value is not revealed and can be arbitrary
     	p[i] = i;
 	}
@@ -169,16 +176,18 @@ uint14 find_min_cost(uint8 *solution) {
 	uint14 min_cost = sum_distance(permutation);
 	uint8 min_array[MAX_WAYPOINTS - 1];
 
-	for (int k = 0; k < MAX_WAYPOINTS - 1; k++) {
+	copy_initial_solution: for (int k = 0; k < MAX_WAYPOINTS - 1; k++) {
 		min_array[k] = permutation[k];
 	}
 
 	p[iterations] = iterations; // p[N] > 0 controls iteration and the index boundary for i
 
 	i = 1;   // setup first swap points to be 1 and 0 respectively (i & j)
-	while (i < iterations) {
+	make_permutations: while (i < iterations) {
     	p[i]--;             // decrease index "weight" for i by one
-        j = i % 2 * p[i];   // IF i is odd then j = p[i] otherwise j = 0
+
+    	// can use logical and instead of modulus
+    	j = !(i & 1) ? 0 : p[i]; // if i is odd then j = p[i] otherwise j = 0
 
         tmp = permutation[j];         // swap(a[j], a[i])
         permutation[j] = permutation[i];
@@ -186,23 +195,25 @@ uint14 find_min_cost(uint8 *solution) {
 
         // We have a new permutation at this point so we are going to test it
         // immediately by summing the distances between the waypoints in it.
-        uint14 cost = sum_distance(permutation);
-        if (cost < min_cost) { // Update the min cost and min path if it is better than before
-        	min_cost = cost;
-        	for (int k = 0; k < MAX_WAYPOINTS - 1; k++) {
-        		min_array[k] = permutation[k];
-        	}
+        if (permutation[0] < permutation[iterations - 1]) {
+            uint14 cost = sum_distance(permutation);
+            if (cost < min_cost) { // Update the min cost and min path if it is better than before
+            	min_cost = cost;
+            	for (int k = 0; k < MAX_WAYPOINTS - 1; k++) {
+            		min_array[k] = permutation[k];
+            	}
+            }
         }
 
         i = 1;              // reset index i to 1 (assumed)
-        while (!p[i]) {     // while (p[i] == 0)
+        increase_odometer: while (!p[i]) {     // while (p[i] == 0)
         	p[i] = i;       // reset p[i] zero value
         	i++;            // set new index value for i (increase by one)
         }
 	}
 
 	// Finally update the solution array to our best answer.
-	for (int k = 0; k < MAX_WAYPOINTS - 1; k++) {
+	copy_solution: for (int k = 0; k < MAX_WAYPOINTS - 1; k++) {
 		solution[k] = min_array[k];
 	}
 	return min_cost;
@@ -216,8 +227,9 @@ void toplevel(hls::stream<uint32> &input, hls::stream<uint32> &output) {
 #pragma HLS INTERFACE ap_ctrl_none port=return
 
 	// Initialise the world.
-	for (int x = 0; x < MAX_WORLD_SIZE; x++) {
-		for (int y = 0; y < MAX_WORLD_SIZE; y++) {
+	init_world_x: for (int x = 0; x < MAX_WORLD_SIZE; x++) {
+		init_world_y: for (int y = 0; y < MAX_WORLD_SIZE; y++) {
+#pragma HLS LOOP_FLATTEN
 			o.nodes[x][y].cost = 0;
 			o.nodes[x][y].status = UNVISITED;
 			o.nodes[x][y].type = NOTHING;
@@ -227,7 +239,8 @@ void toplevel(hls::stream<uint32> &input, hls::stream<uint32> &output) {
 	world.size = input.read(); // Read total world size.
 	world.walls_size = input.read(); // Read number of walls in world.
 
-	for (int i = 0; i < world.walls_size; i++) {
+	make_walls: for (int i = 0; i < world.walls_size; i++) {
+#pragma HLS LOOP_TRIPCOUNT min=6 max=20
 		wall_t wt;
 		uint32 in = input.read(); // Read every wall as a single number for efficiency
 		wt.x = (uint8) (in & 0xFF);
@@ -239,7 +252,9 @@ void toplevel(hls::stream<uint32> &input, hls::stream<uint32> &output) {
 		o.nodes[wt.x][wt.y].type = WALL;
 
 		// Update the type of the nodes according to the wall.
-		for (int j = 0; j < wt.length; j++) {
+		build_wall: for (int j = 0; j < wt.length; j++) {
+#pragma HLS LOOP_TRIPCOUNT min=1 max=60
+#pragma HLS PIPELINE
 			switch (wt.direction) {
 			case HORIZONTAL:
 					if (wt.x + j < world.size) {
@@ -257,7 +272,8 @@ void toplevel(hls::stream<uint32> &input, hls::stream<uint32> &output) {
 
 	world.waypoints_size = input.read(); // Read number of waypoints.
 
-	for (int i = 0; i < world.waypoints_size; i++) {
+	make_waypoints: for (int i = 0; i < world.waypoints_size; i++) {
+#pragma HLS LOOP_TRIPCOUNT min=4 max=12
 		waypoint_t wt;
 		uint32 in = input.read(); // Read waypoint as a single number for efficiency.
 		wt.x = (uint8) (in & 0xFF);
@@ -268,8 +284,11 @@ void toplevel(hls::stream<uint32> &input, hls::stream<uint32> &output) {
 	}
 
 	// first have your system work out the minimum distance between every pair of waypoints
-	for (int i = 0; i < world.waypoints_size; i++) {
-		for (int j = 0; j < world.waypoints_size; j++) {
+	distance_matrix_i: for (int i = 0; i < world.waypoints_size; i++) {
+#pragma HLS LOOP_TRIPCOUNT min=4 max=12
+		distance_matrix_j: for (int j = 0; j < world.waypoints_size; j++) {
+#pragma HLS LOOP_TRIPCOUNT min=4 max=12
+#pragma HLS PIPELINE
 			reset_world();
 			distances[i][j] = a_star(world.waypoints[i], world.waypoints[j]);
 		}
@@ -282,7 +301,8 @@ void toplevel(hls::stream<uint32> &input, hls::stream<uint32> &output) {
 	output.write(total_cost); // Output the length of the path
 
 	// Copy over the waypoints from the calculated solution
-	for (int i = 1; i < world.waypoints_size; i++) {
+	copy_path: for (int i = 1; i < world.waypoints_size; i++) {
+#pragma HLS LOOP_TRIPCOUNT min=4 max=12
 		full_path[i] = path[i - 1];
 	}
 	full_path[world.waypoints_size] = 0; // And append with start/end point
@@ -293,7 +313,7 @@ void toplevel(hls::stream<uint32> &input, hls::stream<uint32> &output) {
 	uint14 distance; // Temporary variable to store the calculate A* distance
 	uint32 out; // Temporary store the value to output to the MicroBlaze software
 	point_t parent;
-	for (int i = 0; i <= world.waypoints_size - 1; i++) {
+	get_path: for (int i = 0; i <= world.waypoints_size - 1; i++) {
 		waypoint_t from = world.waypoints[full_path[i]];
 		waypoint_t to = world.waypoints[full_path[i + 1]];
 
@@ -326,7 +346,7 @@ void toplevel(hls::stream<uint32> &input, hls::stream<uint32> &output) {
 
 		// Now loop over all other coordinates in path.
 		bool found = false;
-		for (;;) {
+		get_parents: for (;;) {
 			// We have reached the goal, hence we can stop.
 			if (parent.x == from.x && parent.y == from.y) break;
 
@@ -358,8 +378,6 @@ void toplevel(hls::stream<uint32> &input, hls::stream<uint32> &output) {
 	out = world.waypoints[0].y;
 	out = (out << 8) | world.waypoints[0].x;
 	output.write(out); // Finally output the start/finish waypoint coordinates.
-
-	reset_world();
 
 	return;
 }
